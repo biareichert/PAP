f0d9a3b20a3cf091d8c4ad68ea1d0f408d31fecd
biareichert <beatrizreichert99@gmail.com>
2019-09-18 15:34:46 -0300

    Update listaa.hs

--- listaa.hs
+++ listaa.hs
@@ -107,3 +107,21 @@ tamanhoLista (x:xs) = 1 + tamanhoLista (xs)
         [1, 2]
 http://wiki.di.uminho.pt/twiki/pub/Education/Archive/ProgramacaoFuncional/PF65-80.pdf
 -}
+
+--foldr
+dobrar_dir :: (a -> b -> b) -> b -> [a] -> b
+dobrar_dir f acc [] = acc
+dobrar_dir f acc (x:xs) =
+    f x (dobrar_dir f acc xs)    
+-- explicação: caso da lista já estiver vazia retornar somente o acumulador
+-- retorna recursivamente a operação (f), acumulador(acc) e a função dobrar_dir com a cauda
+-- imaginar a árvore de ordem de  operação desenhada no quadro
+-- OU fazer o foldr pronto do Haskell: dobrar_dir = foldr
+-- obs: dobrar_dir (+) 0 [1,2,3] = 1 + (2 + (3 + 0))
+
+-- foldl
+dobrar_esq :: (b -> a -> b) -> b -> [a] -> b
+dobrar_esq f acc [] = acc
+dobrar_esq f acc (x:xs) =
+    dobrar_esq f (f acc x) xs
+-- dobrar_esq (+) 0 [1,2,3] = ((0 + 1) + 2) + 3
