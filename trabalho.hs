0e37c2bc5dd927e56dce9aa0cdb0fcd851495e4f
biareichert <beatrizreichert99@gmail.com>
2019-11-22 11:41:46 -0300

    Update trabalho.hs

--- trabalho.hs
+++ trabalho.hs
@@ -11,6 +11,20 @@ type Predicate = (String, [Term])
 type Rule = (Predicate, [Predicate])
 type Substitution = [(Name, Term)]
 
+atom :: Parser Term
+atom = do
+    name <- many1 lower
+    return (Atom name)
+
+variable :: Parser Term
+variable = do
+    name <- many1 upper
+    return (Var (1, name))
+
+parse_type :: Parser Term
+parse_type =
+  try atom <|> variable
+  
 db :: [Rule]
 db = [  (Predicate ("likes", ["ana", "banana"])) ]
 
@@ -43,11 +57,40 @@ substTerm :: Substitution -> Term -> Term
 
 substPred :: Substitution -> Predicate -> Predicate
 
-unifyTerm :: Term -> Term -> Maybe Substitution
+--unifyTerm :: Term -> Term -> Maybe Substitution
 
-unifyPredicate :: Predicate -> Predicate -> Maybe Substitution
+--unifyPredicate :: Predicate -> Predicate -> Maybe Substitution
 
-unifyBody :: [Term] -> [Term] -> Maybe Substitution
+--unifyBody :: [Term] -> [Term] -> Maybe Substitution
+
+unify :: Term -> Term -> Maybe Substitution
+
+-- Regra (REFL)
+unify (Var a) (Var b) | a == b =
+    Just []
+
+-- Regra (LEFT)
+unify (Var a) t | not (occursCheck t a) =
+    Just [(a, t)]
+
+-- Regra (RIGHT)
+-- unify (Term t) (Var a) | not (occursCheck a t) =
+--     Just [(a, t)]
+
+-- Regra (INT)
+unify (Atom a) (Atom b) =
+    Just []
+
+-- Regra (ARROW)
+-- unify (TypeArrow t1 r1) (TypeArrow t2 r2) = do
+--     theta1 <- unify t1 t2
+--     theta2 <- unify (subst theta1 r1) (subst theta1 r2)
+--     --
+--     Just (compose theta2 theta1)
+
+-- Caso geral (não dá pra unificar)
+unify a b =
+    Nothing
 
 occursCheck :: Term -> Name -> Bool
 occursCheck (Atom a) n = 
